72a4065fc3049de702483bacee9c3b845d1820d4
Zaid Ahmed <zaidahmed2345@gmail.com>
2025-02-18 18:02:16 +0500

    Project and Dependecies Setup

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from "@/components/Shared/Header";
+export { default as Footer } from "@/components/Shared/Footer";
+export { default as Button } from "@/components/Shared/Button";
